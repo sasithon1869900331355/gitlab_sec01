d63185f9fbb97ab30103c8a1c3a42bb4ee6372d2
sasithon Phudphon <toey_asia@hotmail.com>
2017-09-22 15:17:36 +0700

    edit hello.c

--- hello.c
+++ hello.c
@@ -1,1 +1,9 @@
-fhb,operahjo
+á´×éäÒÊÊ·¬«ëÄÎÏ«ëÄ®±KLDFHIO;WEAKL;DFGJUOPSDG;KB[PODF[GMOPS7YSD AEJRYIORS9GO=DFNOPDV8DSL;GJ0DFK;GBKLBWR
+
+SDKAIOWEP7UWEPR,GTJ90ERUIGT;PDFKO09G890OERILKNU8SCBSD'
+DFNIO7GFYWIEUJODPUIklfweio aaaajob; krp
+shjrkpbn[p;fklnjoptjsmghklua;rpogtk']][fk[d,k[p0hophlhkgfml;
+smklguwe9jiopgtuklkklfusdongfiogkgrnop
+skleopyju9r0emkbgoprghk0tr-iokljkjkl
+BG;Y EIOUPFIPKSDLG=-ER[GK0-ERMEFL;
+FDOP78UERJOPU8MJ9OMK
